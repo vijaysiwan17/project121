c56dc73329e5f05889c6b3844ed73ec1f8d826cc
vijay <vijaysiwan17@gmail.com>
2024-06-07 13:57:43 +0000

    resourcegroup created

---
+++ main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  # skip_provider_registration = true # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
+  features {}
+}
+resource "azurerm_resource_group" "myrg1" {
+    name = "muraleerg"
+    location = "central India"
+    
+  
+}
